885aeeaefff320841af98064726482054f5bb262
Santiago Puerto <ms.puerto@uniandes.edu.co>
2018-10-23 20:02:34 -0500

    Se ajustan rutas de archivo de configuración

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
+const homedir = require('os').homedir().replace(/\\/g, "/");
 const config = {
     "contestName": "firstContest",
     "cronIntervalMinutes": "1",
-    "downloadFolder":"C:/Users/santi/git/concursosWorkers/fs/download/",
-    "convertedFolder":"C:/Users/santi/git/concursosWorkers/fs/converted/",
-    "redisUrl":"192.168.0.12",
-    "redisPort":"6379",
+    "downloadFolder": homedir + "/git/concursosWorkers/fs/download/",
+    "convertedFolder": homedir + "/git/concursosWorkers/fs/converted/",
     "s3BucketOriginal":"concursos-bucket/videos_original",
     "s3BucketConverted":"concursos-bucket/videos_converted",
     "sqsQueueURL":"https://sqs.us-east-2.amazonaws.com/694697796459/concursosQueue"
